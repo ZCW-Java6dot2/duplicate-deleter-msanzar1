eb6cb530ca20dc7c49936e50d9dff44547e1c8ba
Munir Sanzar <munir@zipcodes-MacBook-Pro-9.local>
2020-11-24 09:16:08 -0500

    passed

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,26 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        return Arrays.stream(array).filter(y -> getOccurrence(y) < maxNumberOfDuplications).toArray(Integer[] :: new);
+    }
+    public Long getOccurrence(int x){
+        return Arrays.stream(array).filter(z -> z==x).count();
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return Arrays.stream(array).filter(y -> getOccurrence(y) != exactNumberOfDuplications).toArray(Integer[]::new);
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,26 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        return Arrays.stream(array).filter(y -> getOccurrence(y) < maxNumberOfDuplications).toArray(String[]::new);
+    }
+    public Long getOccurrence(String x){
+        return Arrays.stream(array).filter(z -> z.equals(x)).count();
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return Arrays.stream(array).filter(y -> getOccurrence(y) != exactNumberOfDuplications).toArray(String[]::new);
+    }
 }
